9c6009798fe707a83a343b55cfee11e81ab3082b
Pratama <pratamasetya99@gmail.com>
2016-11-02 11:48:00 +0700

    add login n register form

---
+++ client/template_back/auth/login.js
@@ -0,0 +1,16 @@
+Template.viewLogin.events({
+	'submit form' : function(event){
+		event.preventDefault();
+		var username = $('[name=username]').val();
+        var password = $('[name=password]').val();
+       
+       	Meteor.loginWithPassword(username, password, function(error){
+		    if(error){
+		        console.log(error.reason);
+		    } else {
+		        Router.go("dashboard");
+		    }
+
+		});
+	}
+});
---
+++ client/template_back/auth/register.js
@@ -0,0 +1,18 @@
+Template.viewRegister.events({
+	'submit form' : function(event){
+		event.preventDefault();
+		var username = $('[name=username]').val();
+        var password = $('[name=password]').val();
+       
+       	Accounts.createUser({
+		    username: username,
+		    password: password
+		}, function(error){
+		    if(error){
+		        console.log(error.reason); // Output error if registration fails
+		    } else {
+		        Router.go("dashboard"); // Redirect user if registration succeeds
+		    }
+		});
+	}
+});
